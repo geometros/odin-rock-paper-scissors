2d317193499d3fe45b0b72c0f45d34437b506f04
Paul Christianson <pacdeveloping@gmail.com>
2024-05-09 16:25:08 -0400

    Added getUserChoice function

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function GetComputerChoice() {
+function getComputerChoice() {
     let x = Math.random();
     if (x < 1/3) {return "Rock"}
     if (x > 2/3) {return "Paper"}
     else {return "Scissors"}
 }
-console.log(GetComputerChoice())
+console.log(getComputerChoice())
+
+function getUserChoice() {
+    while (true){
+        let input = prompt('Rock, Paper, Scissors?');
+        lowerInput = input.toLowerCase()
+        if (lowerInput == 'rock'){return 'Rock'}
+        if (lowerInput == 'paper'){return 'Paper'}
+        if (lowerInput == 'scissors'){return 'Scissors'}
+    }
+}
+
+console.log(getUserChoice())
