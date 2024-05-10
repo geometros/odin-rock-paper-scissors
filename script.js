cc41887633eb074982e6f18b6ed374691a02aca4
Paul Christianson <pacdeveloping@gmail.com>
2024-05-09 20:46:28 -0400

    Created playGame function to wrap playRound, game is working

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function getComputerChoice() {
     if (x > 2/3) {return "Paper"}
     else {return "Scissors"}
 }
-console.log(getComputerChoice())
 
 function getUserChoice() {
     while (true){
@@ -16,28 +15,48 @@ function getUserChoice() {
     }
 }
 
-console.log(getUserChoice())
-
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(playerChoice, computerChoice){
-    if (playerChoice == computerChoice){return 'Tie!'}
-    if (playerChoice == 'Rock'){
-        if (computerChoice == 'Scissors'){playerScore += 1;return 'Rock beats Scissors!'}
-        if (computerChoice == 'Paper'){computerScore += 1;return 'Paper beats Rock!'}
+function playGame(){
+
+    function playRound(playerChoice, computerChoice){
+
+        console.log(`Computer chooses ${computerChoice}`)
+        console.log(`Player chooses ${playerChoice}`)
+
+        if (playerChoice == computerChoice){return 'Tie!'}
+        if (playerChoice == 'Rock'){
+            if (computerChoice == 'Scissors'){playerScore += 1;return 'Rock beats Scissors!'}
+            if (computerChoice == 'Paper'){computerScore += 1;return 'Paper beats Rock!'}
+        }
+        if (playerChoice == 'Paper'){
+            if (computerChoice == 'Rock'){playerScore += 1;return 'Paper beats Rock!'}
+            if (computerChoice == 'Scissors'){computerScore += 1;return 'Scissors beats Paper!'}
+        }
+        if (playerChoice == 'Scissors'){
+            if (computerChoice == 'Paper'){playerScore += 1;return 'Scissors beats Paper!'}
+            if (computerChoice == 'Rock'){computerScore += 1;return 'Rock beats Scissors!'}
+        }
     }
-    if (playerChoice == 'Paper'){
-        if (computerChoice == 'Rock'){playerScore += 1;return 'Paper beats Rock!'}
-        if (computerChoice == 'Scissors'){computerScore += 1;return 'Scissors beats Paper!'}
-    }
-    if (playerChoice == 'Scissors'){
-        if (computerChoice == 'Paper'){playerScore += 1;return 'Scissors beats Paper!'}
-        if (computerChoice == 'Rock'){computerScore += 1;return 'Rock beats Scissors!'}
-    }
+
+    const playerSelection = getUserChoice();
+    const computerSelection = getComputerChoice();
+
+    console.log(playRound(playerSelection, computerSelection))
+    console.log(`Player Score: ${playerScore}`)
+    console.log(`Computer Score: ${computerScore}`)
 }
 
-const playerSelection = getUserChoice();
-const computerSelection = getComputerChoice();
-
-playRound(playerSelection, computerSelection)
+for (let i=0;i<5;i++){
+    playGame()
+}
+if (playerScore == computerScore){
+    console.log('The game was a tie')
+}
+else if (playerScore > computerScore) {
+    console.log('The Player wins!')
+} 
+else {
+    console.log('The Computer wins!')
+}
