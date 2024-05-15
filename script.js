4e20490005f4b42195230cf1117eb0bfdb67b085
Paul Christianson <pacdeveloping@gmail.com>
2024-05-14 23:46:33 -0400

    Removed playGame function entirely, got player input working with buttons, but game logic needs work

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+const roundsPerGame = 5;
+
+let round = 1;
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerChoice, computerChoice){
+
+    console.log(`Computer chooses ${computerChoice}`)
+    console.log(`Player chooses ${playerChoice}`)
+
+    if (playerChoice == computerChoice){return 'Tie!'}
+    if (playerChoice == 'Rock'){
+        if (computerChoice == 'Scissors'){playerScore += 1;return 'Rock beats Scissors!'}
+        if (computerChoice == 'Paper'){computerScore += 1;return 'Paper beats Rock!'}
+    }
+    if (playerChoice == 'Paper'){
+        if (computerChoice == 'Rock'){playerScore += 1;return 'Paper beats Rock!'}
+        if (computerChoice == 'Scissors'){computerScore += 1;return 'Scissors beats Paper!'}
+    }
+    if (playerChoice == 'Scissors'){
+        if (computerChoice == 'Paper'){playerScore += 1;return 'Scissors beats Paper!'}
+        if (computerChoice == 'Rock'){computerScore += 1;return 'Rock beats Scissors!'}
+    }
+    round += 1;
+    if (round > roundsPerGame){endGame()}
+}
+
+//console.log(playRound(playerSelection, computerSelection)) //need to fix this to grab button selection below
+console.log(`Player Score: ${playerScore}`)
+console.log(`Computer Score: ${computerScore}`)   
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => playRound(button.className,getComputerChoice()))
+})
+
 function getComputerChoice() {
     let x = Math.random();
     if (x < 1/3) {return "Rock"}
@@ -5,58 +42,14 @@ function getComputerChoice() {
     else {return "Scissors"}
 }
 
-function getUserChoice() {
-    while (true){
-        let input = prompt('Rock, Paper, Scissors?');
-        lowerInput = input.toLowerCase()
-        if (lowerInput == 'rock'){return 'Rock'}
-        if (lowerInput == 'paper'){return 'Paper'}
-        if (lowerInput == 'scissors'){return 'Scissors'}
+function endGame() {
+    if (playerScore == computerScore){
+        console.log('The game was a tie')
     }
-}
-
-let playerScore = 0;
-let computerScore = 0;
-
-function playGame(){
-
-    function playRound(playerChoice, computerChoice){
-
-        console.log(`Computer chooses ${computerChoice}`)
-        console.log(`Player chooses ${playerChoice}`)
-
-        if (playerChoice == computerChoice){return 'Tie!'}
-        if (playerChoice == 'Rock'){
-            if (computerChoice == 'Scissors'){playerScore += 1;return 'Rock beats Scissors!'}
-            if (computerChoice == 'Paper'){computerScore += 1;return 'Paper beats Rock!'}
-        }
-        if (playerChoice == 'Paper'){
-            if (computerChoice == 'Rock'){playerScore += 1;return 'Paper beats Rock!'}
-            if (computerChoice == 'Scissors'){computerScore += 1;return 'Scissors beats Paper!'}
-        }
-        if (playerChoice == 'Scissors'){
-            if (computerChoice == 'Paper'){playerScore += 1;return 'Scissors beats Paper!'}
-            if (computerChoice == 'Rock'){computerScore += 1;return 'Rock beats Scissors!'}
-        }
+    else if (playerScore > computerScore) {
+        console.log('The Player wins!')
+    } 
+    else {
+        console.log('The Computer wins!')
     }
-
-    const playerSelection = getUserChoice();
-    const computerSelection = getComputerChoice();
-
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(`Player Score: ${playerScore}`)
-    console.log(`Computer Score: ${computerScore}`)
-}
-
-while (true){
-    playGame()
-}
-if (playerScore == computerScore){
-    console.log('The game was a tie')
-}
-else if (playerScore > computerScore) {
-    console.log('The Player wins!')
-} 
-else {
-    console.log('The Computer wins!')
 }
