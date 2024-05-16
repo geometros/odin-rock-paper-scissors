c95b28c90f2d776bc60618b0522c1bf75153b371
Paul Christianson <pacdeveloping@gmail.com>
2024-05-15 21:30:08 -0400

    set up div to display round results in browser

--- script.js
+++ script.js
@@ -6,24 +6,37 @@ let computerScore = 0;
 
 function playRound(playerChoice, computerChoice){
 
-    console.log(`Computer chooses ${computerChoice}`)
-    console.log(`Player chooses ${playerChoice}`)
+    
+    let roundResult = null;
+    let compChoiceDisplay = document.createElement('p');
+    compChoiceDisplay.textContent = `Computer chooses ${computerChoice}`;
+    let playChoiceDisplay = document.createElement('p');
+    playChoiceDisplay.textContent = `Player chooses ${playerChoice}`;
 
-    if (playerChoice == computerChoice){return 'Tie!'}
+    if (playerChoice == computerChoice){
+        return 'Tie!'
+    }
     if (playerChoice == 'Rock'){
-        if (computerChoice == 'Scissors'){playerScore += 1;return 'Rock beats Scissors!'}
-        if (computerChoice == 'Paper'){computerScore += 1;return 'Paper beats Rock!'}
+        if (computerChoice == 'Scissors'){playerScore += 1; roundResult = 'Rock beats Scissors!'}
+        if (computerChoice == 'Paper'){computerScore += 1;roundResult = 'Paper beats Rock!'}
     }
     if (playerChoice == 'Paper'){
-        if (computerChoice == 'Rock'){playerScore += 1;return 'Paper beats Rock!'}
-        if (computerChoice == 'Scissors'){computerScore += 1;return 'Scissors beats Paper!'}
+        if (computerChoice == 'Rock'){playerScore += 1;roundResult = 'Paper beats Rock!'}
+        if (computerChoice == 'Scissors'){computerScore += 1;roundResult = 'Scissors beats Paper!'}
     }
     if (playerChoice == 'Scissors'){
-        if (computerChoice == 'Paper'){playerScore += 1;return 'Scissors beats Paper!'}
-        if (computerChoice == 'Rock'){computerScore += 1;return 'Rock beats Scissors!'}
+        if (computerChoice == 'Paper'){playerScore += 1;roundResult = 'Scissors beats Paper!'}
+        if (computerChoice == 'Rock'){computerScore += 1;roundResult = 'Rock beats Scissors!'}
     }
+
+    const div = document.querySelector('#results')
+    div.innerHTML = roundResult;
+
     round += 1;
-    if (round > roundsPerGame){endGame()}
+    console.log(round)
+    if (round > roundsPerGame){
+        endGame()
+    }
 }
 
 //console.log(playRound(playerSelection, computerSelection)) //need to fix this to grab button selection below
